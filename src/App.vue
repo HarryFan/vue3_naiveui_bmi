1f654e03fd48ae75977cad503cb2db69b9c50a63
Harry <harry750110@gmail.com>
2023-03-14 18:23:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import BMI from './components/BMI.vue'
+</script>
+
+<template>
+
+  <div class="logo-container">
+    <img src="./assets/vue.svg" alt="" class="logo">
+    <BMI />
+  </div>
+</template>
+
+<style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+</style>
